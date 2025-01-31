8375a89778bcf78bbf8cf93be83fb4ca6c533c55
Danil Kladov <kladov113@yandex.com>
2025-01-31 15:28:50 +0300

    redirect

--- backend/api/views.py
+++ backend/api/views.py
@@ -143,6 +143,13 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = None
 
 
+from rest_framework.generics import RetrieveAPIView
+
+class RecipeDetailView(RetrieveAPIView):
+    """Представление для просмотра деталей рецепта."""
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     """Вьюсет для Ингредиентов."""
 
