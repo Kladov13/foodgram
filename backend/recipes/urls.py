69912ebe5e49af479ee92d554341883ed6259a62
Danil Kladov <kladov113@yandex.com>
2025-02-02 21:19:00 +0300

    urls done

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 
-from api.views import RecipeDetailView
 from .views import recipe_redirect
 
 urlpatterns = [
-    path('recipes/<int:pk>/',
-         recipe_redirect, name='recipe-detail'),
     path('s/<int:pk>/', recipe_redirect, name='recipe-redirect'),
 ]
