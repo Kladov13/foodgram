9ecd448f9362a94795000c601a48b01a4c8722f9
Danil Kladov <kladov113@yandex.com>
2025-02-02 21:28:14 +0300

    try urls 2

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -5,5 +5,5 @@ from .views import recipe_redirect
 urlpatterns = [
     path('recipes/<int:pk>/',
          recipe_redirect, name='recipe-detail'),
-    path('s/<int:pk>/', recipe_redirect, name='recipe-redirect'),
+
 ]
